45e2add450cfdee3e308b541b8c3f46a9bec1b11
Jackson Curtis <jcurtis210@gmail.com>
2017-03-29 02:41:01 +0000

    started working on the API parsing

--- routes/index.js
+++ routes/index.js
@@ -11,18 +11,145 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+var fs =require('fs');
+var readline = require('readline');
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
+
+// If modifying these scopes, delete your previously saved credentials
+// at ~/.credentials/gmail-nodejs-quickstart.json
+var SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
+var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+    process.env.USERPROFILE) + '/.credentials/';
+var TOKEN_PATH = TOKEN_DIR + 'gmail-nodejs-quickstart.json';
+//console.log(TOKEN_PATH);
+// Load client secrets from a local file.
+fs.readFile('client_secret.json', function processClientSecrets(err, content) {
+  if (err) {
+    console.log('Error loading client secret file: ' + err);
+    return;
+  }
+  // Authorize a client with the loaded credentials, then call the
+  // Gmail API.
+  authorize(JSON.parse(content), getAllEmails);
+});
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ *
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+  var clientSecret = credentials.installed.client_secret;
+  var clientId = credentials.installed.client_id;
+  var redirectUrl = credentials.installed.redirect_uris[0];
+  var auth = new googleAuth();
+  var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, function(err, token) {
+    if (err) {
+      getNewToken(oauth2Client, callback);
+    } else {
+      oauth2Client.credentials = JSON.parse(token);
+      callback(oauth2Client);
+    }
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ *
+ * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback to call with the authorized
+ *     client.
+ */
+function getNewToken(oauth2Client, callback) {
+  var authUrl = oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES
+  });
+  console.log('Authorize this app by visiting this url: ', authUrl);
+  var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.question('Enter the code from that page here: ', function(code) {
+    rl.close();
+    oauth2Client.getToken(code, function(err, token) {
+      if (err) {
+        console.log('Error while trying to retrieve access token', err);
+        return;
+      }
+      oauth2Client.credentials = token;
+      storeToken(token);
+      callback(oauth2Client);
+    });
+  });
+}
+
+/**
+ * Store token to disk be used in later program executions.
+ *
+ * @param {Object} token The token to store to disk.
+ */
+function storeToken(token) {
+  try {
+    fs.mkdirSync(TOKEN_DIR);
+  } catch (err) {
+    if (err.code != 'EEXIST') {
+      throw err;
+    }
+  }
+  fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+  console.log('Token stored to ' + TOKEN_PATH);
+}
+
+/**
+ * Lists the labels in the user's account.
+ *
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+
+
+function getAllEmails(auth) {
+  var gmail = google.gmail('v1');
+//console.log(auth);
+  gmail.users.messages.list({
+    auth: auth,
+	id: "15b17c3026b71ac8",
+    userId: 'me',
+  }, function(err, response) {
+    if (err) {
+      console.log('The API returned an error: ' + err);
+      return;
+    }
+    if(response.resultSizeEstimate>0){
+for (i in response.messages)
+{
+console.log(i);
+}
+}
+      });
+    }
+ 
+
 
 //var minutes=.2, the_interval = minutes * 60 * 1000;
 //setInterval(function() {
-  console.log("Running regularly");
+/*  console.log("Running regularly");
 var getOptions={
     type: "GET",
     dataType: 'text',
-    url: api+'/'+id+'/profile',
+auth: auth,
+    url: api+'/me'+'/profile',
     crossDomain : true
 } 
 console.log(api+"/me/profile")
-request(api+"/me/profile", function(err, resp, body) {
+request(getOptions, function(err, resp, body) {
 if(err){
 //console.log(err);
 }
@@ -32,5 +159,5 @@ resp.on('data', function(chunk){
 console.log('Response: '+chunk);
 });
 });
-//}, the_interval);
+//}, the_interval);*/
 module.exports = router;
